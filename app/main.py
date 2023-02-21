83ba94cb98481c418398060f85b21e0438a0a1d5
madushan <ranasinhemadushan@gmail.com>
2023-02-22 02:37:48 +0530

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import  FastAPI
 from pydantic import BaseModel
 from .routers import chat
 from .routers import feedback
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000/"], # set to "*" to allow all origins or replace with a list of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"], # set to "*" to allow all methods or replace with a list of allowed methods
+    allow_headers=["*"], # set to "*" to allow all headers or replace with a list of allowed headers
+)
 
 app.include_router(chat.router)
 app.include_router(feedback.router)
