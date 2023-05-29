a8a8ede12083e08bdd1aabe387ad45429c60bde6
madushan <ranasinhemadushan@gmail.com>
2023-05-29 23:37:20 +0530

    config change

--- app/config.py
+++ app/config.py
@@ -4,19 +4,19 @@ import os
 
 
 class Settings(BaseSettings):
-    # db_url: str = os.getenv("MONGO_URL", "")
-    # db_name: str = os.getenv("MONGO_DB", "")
-    # collection: str = os.getenv("MONGO_COLLECTION", "")
-    # collection2: str = os.getenv("MONGO_COLLECTION2", "")
-    # client_origin: str = os.getenv("CLIENT_ORIGIN", "")
-    # weather_apiKey: str = os.getenv("OPEN_WEATHER_API_KEY", "")
+    db_url: str = os.getenv("MONGO_URL", "")
+    db_name: str = os.getenv("MONGO_DB", "")
+    collection: str = os.getenv("MONGO_COLLECTION", "")
+    collection2: str = os.getenv("MONGO_COLLECTION2", "")
+    client_origin: str = os.getenv("CLIENT_ORIGIN", "")
+    weather_apiKey: str = os.getenv("OPEN_WEATHER_API_KEY", "")
 
-    db_url: str = ""
-    db_name: str = ""
-    collection: str = ""
-    collection2: str = ""
-    client_origin: str = ""
-    weather_apiKey: str = ""
+    # db_url: str = ""
+    # db_name: str = ""
+    # collection: str = ""
+    # collection2: str = ""
+    # client_origin: str = ""
+    # weather_apiKey: str = ""
 
     class Config:
         env_file = ".env"
