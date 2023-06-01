01f60b5a82f64263888d6e94cb42baff76ccf56e
madushan <ranasinhemadushan@gmail.com>
2023-06-01 17:48:15 +0530

    load env configured , health bot clac_condition removed

--- app/internal/functions/functions.py
+++ app/internal/functions/functions.py
@@ -1,33 +1,17 @@
-from pytz import timezone 
+from pytz import timezone
 from datetime import datetime
 from .weather import getWeather
 
+
 def getTime():
-    ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('දැන් වෙලාව %H:%M')
-    print(ind_time)
-    return ind_time
-
-
+    ind_time = datetime.now(timezone("Asia/Kolkata"))
+    datetime_object = datetime.fromisoformat(str(ind_time))
+    date = datetime_object.date()
+    time = datetime_object.time().strftime("%H:%M:%S")
+    return "දැන් වෙලාව" + " " + str(time)
 
 
 get_func = {
     'time': getTime,
     'weather': getWeather
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
